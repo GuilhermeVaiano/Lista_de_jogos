7a34b02357f82f41b522ef4d9095a9c30f6ec13b
Guilherme Vaiano <guivnmendonca@gmail.com>
2023-05-10 21:41:14 -0300

    Seed das tabelas e busca por id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -15,8 +17,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	// Retorna uma lista preenchendo o GameMinDTO de acordo com as 
+	// infos adquiridas do Game, copiando apenas os campos solicitados.
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true) // Assegura que não havera acoes escritas
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
