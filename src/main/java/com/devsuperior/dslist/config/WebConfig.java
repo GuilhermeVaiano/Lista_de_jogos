dc549d83c58942822c9eca01b6b0a05799c6cdc5
Guilherme Vaiano <guivnmendonca@gmail.com>
2023-05-12 12:53:14 -0300

    cors configurado

---
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Classe que permite o acesso dos enderecos colocados na variavel.
+
+@Configuration
+public class WebConfig {
+
+	@Value("${cors.origins}") // pega a variavel do projeto
+	private String corsOrigins;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
+	}
+	
+}
